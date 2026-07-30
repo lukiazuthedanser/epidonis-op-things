97480a4b96d3e801ee41c0a0f10320da883f1b7b
lukiazuthedanser <lukiazu31@gmail.com>
2026-07-30 18:53:43 +0800

    Add serpulo.js with planet configuration

---
+++ scripts/serpulo.js
@@ -0,0 +1,7 @@
+//Planets.serpulo.allowSectorInvasion = true;  Removed by convenience.
+Planets.serpulo.atmosphereColor = Color.valueOf('#ffee22');
+Planets.serpulo.orbitRadius = 70;
+Planets.serpulo.ruleSetter = (r) => {
+	r.hideSpawns = false;
+	r.deconstructRefundMultiplier = 1.99;
+};
